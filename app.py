2359acacde37e12a3ecbfe68e5d5dd56a1d755a8
David Birks <david@birks.dev>
2024-08-20 12:57:17 +0000

    Get the text area working

--- app.py
+++ app.py
@@ -11,17 +11,13 @@ RUNWARE_API_KEY = os.getenv("RUNWARE_API_KEY")
 RUNWARE_LOG_LEVEL = os.getenv("RUNWARE_LOG_LEVEL")
 
 
-async def fetch_images() -> list:
+async def fetch_images(prompt: str) -> list:
     try:
         runware = Runware(api_key=RUNWARE_API_KEY, log_level=RUNWARE_LOG_LEVEL)
         await runware.connect()
 
         request_image = IImageInference(
-            positivePrompt="""
-            a group of settlers looking out over a vast plain, 
-            wondering where the next meal will come from, storm on the horizon, 
-            and for some reason they're all wearing bright cyan shirts with pink polka dots
-            """,
+            positivePrompt=prompt,
             model="runware:100@1",
             numberResults=1,
             # negativePrompt="cloudy, rainy",
@@ -42,13 +38,18 @@ def main():
 
     st.title("Runware UI")
 
-    try:
-        images = asyncio.run(fetch_images())
-        for image in images:
-            print(f"Image: {image.imageURL}")
-            st.image(image.imageURL)
-    except Exception as e:
-        print(f"An error occurred in main: {e}")
+    placeholder_prompt="a group of settlers looking out over a vast plain"
+
+    prompt_text_box = st.text_area(label="Enter your prompt here:", value=placeholder_prompt)
+
+    if st.button("Submit", type="primary"):
+        try:
+            images = asyncio.run(fetch_images(prompt=prompt_text_box))
+            for image in images:
+                print(f"Image: {image.imageURL}")
+                st.image(image.imageURL)
+        except Exception as e:
+            print(f"An error occurred in main: {e}")
 
 
 if __name__ == "__main__":
